9d36d85ec2146a63d6d37ae038677095858ed455
Johan Reitan <johan.reitan@gmail.com>
2021-02-15 20:35:22 +0100

    disable clustering at zoom threshold

--- app/src/main/java/app/reitan/nearby_mobility/features/map/ScooterRenderer.kt
+++ app/src/main/java/app/reitan/nearby_mobility/features/map/ScooterRenderer.kt
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.BitmapDescriptor
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.clustering.Cluster
 import com.google.maps.android.clustering.ClusterManager
 import com.google.maps.android.clustering.view.DefaultClusterRenderer
 
@@ -17,13 +18,19 @@ class ScooterRenderer(
     clusterManager: ClusterManager<ScooterClusterItem>,
 ) : DefaultClusterRenderer<ScooterClusterItem>(context, map, clusterManager) {
 
+    private var zoom = map.cameraPosition.zoom
+
     private val rydeIcon = bitmapDescriptorFromVector(context, R.drawable.ic_map_pin_ryde)
     private val tierIcon = bitmapDescriptorFromVector(context, R.drawable.ic_map_pin_tier)
     private val voiIcon = bitmapDescriptorFromVector(context, R.drawable.ic_map_pin_voi)
     private val unknownIcon = bitmapDescriptorFromVector(context, R.drawable.ic_map_pin_unknown)
 
+    init {
+        map.setOnCameraMoveListener { zoom = map.cameraPosition.zoom }
+    }
+
     private val ScooterClusterItem.icon: BitmapDescriptor
-        get() = when(scooter.operator) {
+        get() = when (scooter.operator) {
             Operator.Voi -> voiIcon
             Operator.Tier -> tierIcon
             Operator.Ryde -> rydeIcon
@@ -32,12 +39,16 @@ class ScooterRenderer(
 
     override fun onBeforeClusterItemRendered(
         item: ScooterClusterItem,
-        markerOptions: MarkerOptions
+        markerOptions: MarkerOptions,
     ) {
         markerOptions.icon(item.icon)
     }
 
     override fun onClusterItemUpdated(item: ScooterClusterItem, marker: Marker) {
         marker.setIcon(item.icon)
+    }
+
+    override fun shouldRenderAsCluster(cluster: Cluster<ScooterClusterItem>): Boolean {
+        return zoom < 15 && super.shouldRenderAsCluster(cluster)
     }
 }
