c4fb572cdca5c8c199039e0df1f5998ea10f0796
Johan Reitan <johan.reitan@gmail.com>
2021-02-15 22:56:46 +0100

    set background color

--- app/src/main/java/app/reitan/nearby_mobility/App.kt
+++ app/src/main/java/app/reitan/nearby_mobility/App.kt
@@ -1,7 +1,8 @@
 package app.reitan.nearby_mobility
 
-import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import app.reitan.nearby_mobility.features.map.ScooterMap
 import app.reitan.nearby_mobility.tools.LocationResult
@@ -10,7 +11,7 @@ import app.reitan.nearby_mobility.tools.lastLocation
 @Composable
 fun App() {
     when (val lastLocation = lastLocation()) {
-        LocationResult.Loading -> Scaffold {}
+        LocationResult.Loading -> Surface(color = colorResource(R.color.ic_launcher_background)) {}
         is LocationResult.Success -> {
             ScooterMap(location = lastLocation.location)
         }
