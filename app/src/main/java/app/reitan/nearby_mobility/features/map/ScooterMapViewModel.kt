33ec1376dd89bd29368ff20efb8ad95cadf83649
Johan Reitan <johan.reitan@gmail.com>
2021-02-21 22:17:23 +0100

    ignore errors

--- app/src/main/java/app/reitan/nearby_mobility/features/map/ScooterMapViewModel.kt
+++ app/src/main/java/app/reitan/nearby_mobility/features/map/ScooterMapViewModel.kt
@@ -1,10 +1,14 @@
 package app.reitan.nearby_mobility.features.map
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import app.reitan.common.Repository
+import kotlinx.coroutines.flow.catch
 
 class ScooterMapViewModel(repo: Repository) : ViewModel() {
     val visibleRegion = repo.visibleRegion
 
     val scooters = repo.scooters
+        // TODO: handle errors properly
+        .catch { Log.e("NearbyMobility", "Fetching scooters failed", it) }
 }
